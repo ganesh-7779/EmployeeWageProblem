3356bc4346cee03221a23ec40bd9715c0529b5eb
Ganesh <ganeshgavhad9@gmail.com>
2021-05-31 18:41:24 +0530

    UC1

---
+++ EmpWage.java
@@ -0,0 +1,15 @@
+
+public class EmpWage {
+	public static void main (String []args) {
+		
+		int fullTime = 1;
+		double empCheck= Math.floor(Math.random()*10)%2;
+			System.out.println("Welcome to empolyee wage Computation");
+			if ( fullTime == empCheck )
+				System.out.println("Emp is Present");
+			else
+				System.out.println("emp is absent");
+			
+		}
+	}
+
