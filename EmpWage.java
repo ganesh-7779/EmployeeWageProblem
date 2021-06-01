e3857e66283397046bdf15e4e1016159e1945e7a
Ganesh <ganeshgavhad9@gmail.com>
2021-06-01 08:41:58 +0530

    EmpWage

--- EmpWage.java
+++ EmpWage.java
@@ -10,6 +10,45 @@ public class EmpWage {
 			else
 				System.out.println("emp is absent");
 			
+			//Daily wage calculation
+			
+			int WagePerHour = 20;
+			int FullDayHours = 8;
+			int PartTimeHours = 4;
+			int PartTime = 1;
+			int MonthDay= 20;
+			int Month = 0;
+			int MaxHours = 0;
+			int WorkingHrs = 0;
+			int month = Month + 1;
+			int maxHours = MaxHours + WorkingHrs ;
+			if(fullTime == empCheck)
+			
+			System.out.println("DailyWage : " + WagePerHour*FullDayHours);
+			
+			else
+				System.out.println("No Daily Wage, Employee is Absend");
+			
+			if (PartTime == empCheck)
+				System.out.println("Part Time Wage :" + PartTimeHours*WagePerHour);
+			else
+				
+				System.out.println("No Part Time  Wage, Employee is Absend");
+			
+			if(Month == empCheck)
+				
+				System.out.println("Month Wage : " + WagePerHour*FullDayHours*MonthDay);
+
+			else
+				System.out.println("No Monthly  Wage, Employee is Absend");
+			
+			if(maxHours == 100 && month == 20 )	{
+				System.out.println("Monthly Wage : " + MonthDay*MaxHours*WagePerHour);
+			break;
+			
+			else
+				System.out.println("No Monthly wage,employee is absent" + Month);
+		
 		}
 	}
-
+}
