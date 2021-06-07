6512527b250a219effa568fb52674c151725b704
Ganesh <ganeshgavhad9@gmail.com>
2021-06-07 12:49:28 +0530

    Added:EmpWage By OOP Concept

--- EmpWage.java
+++ EmpWage.java
@@ -1,54 +1,104 @@
 
 public class EmpWage {
-	public static void main (String []args) {
+	
+	final static int WAGE_PER_HRS = 20;
+	final static int FULL_DAY_HRS = 8;
+	final static int PART_TIME_HRS = 4;
+	final static int FULL_TIME_HRS = 8;
+	final static int PART_TIME = 1;
+	final static int FULL_TIME=2;
+	final static int DAY_PER_MONTH=20;
+	final static int MAX_HRS=100;
+	
+	/**
+	 * checking for employee is present or absent
+	 */
+	private static void Attaindence() {
 		
 		int fullTime = 1;
 		double empCheck= Math.floor(Math.random()*10)%2;
-			System.out.println("Welcome to empolyee wage Computation");
 			if ( fullTime == empCheck )
-				System.out.println("Emp is Present");
-			else
-				System.out.println("emp is absent");
-			
-			//Daily wage calculation
-			
-			int WagePerHour = 20;
-			int FullDayHours = 8;
-			int PartTimeHours = 4;
-			int PartTime = 1;
-			int MonthDay= 20;
-			int Month = 0;
-			int MaxHours = 0;
-			int WorkingHrs = 0;
-			int month = Month + 1;
-			int maxHours = MaxHours + WorkingHrs ;
-			if(fullTime == empCheck)
-			
-			System.out.println("DailyWage : " + WagePerHour*FullDayHours);
-			
-			else
-				System.out.println("No Daily Wage, Employee is Absend");
-			
-			if (PartTime == empCheck)
-				System.out.println("Part Time Wage :" + PartTimeHours*WagePerHour);
-			else
-				
-				System.out.println("No Part Time  Wage, Employee is Absend");
-			
-			if(Month == empCheck)
-				
-				System.out.println("Month Wage : " + WagePerHour*FullDayHours*MonthDay);
+				System.out.println("emp is present");
 
 			else
-				System.out.println("No Monthly  Wage, Employee is Absend");
-			
-			if(maxHours == 100 && month == 20 )	{
-				System.out.println("Monthly Wage : " + MonthDay*MaxHours*WagePerHour);
-			break;
-			
-			else
-				System.out.println("No Monthly wage,employee is absent" + Month);
+				System.out.println("emp is absent");
+	}
+	
+	/**
+	 * @return daily employee wage
+	 */
+	private static double CalculateDailyWage() {
+		int empCheck= (int) Math.floor(Math.random()*10)%2+1;
+		double empWage=0;
+		switch (empCheck) {
 		
+		case FULL_TIME:
+			empWage=FULL_TIME_HRS*WAGE_PER_HRS;
+			break;
+		case PART_TIME:
+			empWage=PART_TIME_HRS*WAGE_PER_HRS;
+			break;
+		}	
+		
+		return empWage;	
+	}	
+	/**
+	 * @return employee monthly wage
+	 */
+	private static double monthlyWage() {
+		
+			int empCheck= (int) Math.floor(Math.random()*10)%2+1;
+			double monthlyWage=0;
+			switch (empCheck) {
+			
+			case FULL_TIME:
+				monthlyWage=FULL_TIME*WAGE_PER_HRS*DAY_PER_MONTH;
+				break;
+			case PART_TIME:
+				monthlyWage=PART_TIME_HRS*WAGE_PER_HRS*DAY_PER_MONTH;
+				break;
+			}
+			return monthlyWage;
+		}
+	/**
+	 * till max condition reach
+	 * @return maximum wage
+	 */
+	private static double maxEmpWage() {
+		
+		int empCheck= (int) Math.floor(Math.random()*10)%2+1;
+		double maxWage=0;
+		int countHrs=0;
+		int countDay=0;
+		while (countHrs<MAX_HRS && countDay<DAY_PER_MONTH) {
+			switch (empCheck) {
+			
+			case FULL_TIME:
+				maxWage=FULL_TIME*WAGE_PER_HRS*DAY_PER_MONTH;
+				maxWage+=maxWage;
+				countHrs+=8;
+				countDay++;
+				
+				break;
+			case PART_TIME:
+				maxWage=PART_TIME_HRS*WAGE_PER_HRS*DAY_PER_MONTH;
+				maxWage+=maxWage;
+				countHrs+=4;
+				countDay++;
+				break;
+			}
+		}
+		
+		return maxWage;
+	}
+	public static void main (String []args) {
+		
+		System.out.println("welcome to employeewage");
+		Attaindence();
+		System.out.println(CalculateDailyWage());
+		System.out.println(monthlyWage());
+		System.out.println(maxEmpWage());
+
 		}
 	}
-}
+
